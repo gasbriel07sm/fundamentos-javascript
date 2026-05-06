d539d8a1c470d89363fb2d82a4e386d1afcb97a2
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-06 12:27:30 -0300

    Aula 026 - operador ternário

---
+++ aula026/index.js
@@ -0,0 +1,9 @@
+/**
+ * Operador Ternário 
+ * <condição> ? true : false
+ */
+
+const pontuacaoUsuario = 999;
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUsuario); 
