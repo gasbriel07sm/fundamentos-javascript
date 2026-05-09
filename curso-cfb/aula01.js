401ccaffbc7010baff867ac6d225b5dcf0dc8ce4
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-09 17:55:24 -0300

    curso cfb -aula 01

---
+++ curso-cfb/aula01.js
@@ -0,0 +1,6 @@
+// Comentários em JavaScript é o // (comentário de linha)
+/** Comentário de bloco (mais de uma linha)
+ *  console.log(comentado)
+ */
+console.log('Hello world!');
+// comando de saída
