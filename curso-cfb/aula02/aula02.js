0ab9e25dbaa242fe454ab5f634f295f9d41e1929
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-09 18:01:35 -0300

    curso cfb -aula 02

---
+++ curso-cfb/aula02/aula02.js
@@ -0,0 +1,9 @@
+/** Modo estrito de JavaScript - deixar nosso código mais limpo
+ *  Não conseguimos usar uma variável por exemplo sem declarar ela
+ */
+"use strict" // modo restrito
+
+let nome = "Bruno"; // declarando variáveis
+console.log("CFB Cursos");
+console.log(nome);
+console.log("Nome " + nome);
