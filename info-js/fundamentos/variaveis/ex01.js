7acab57129e41592c68f14dfab6779cba0b5355b
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-19 12:56:57 -0300

    Fundamentos - Variáveis

---
+++ info-js/fundamentos/variaveis/ex01.js
@@ -0,0 +1,9 @@
+// declarando duas variáveis sem valor até o momento
+let admin, nome;
+
+// atribuindo valor para as variáveis
+nome = 'John';
+admin = nome;
+
+console.log(admin);
+console.log(nome);
